1dec97697ccda5558bbd09b8f285a7d3baf9ca27
Inna-Ser <serebryakovainna@mail.ru>
2023-07-29 11:01:57 -0500

    correct hw-3.js

--- hw-3.js
+++ hw-3.js
@@ -1,45 +1,51 @@
-/*let password = 'Пароль';
-let password2 = prompt ('Введите пароль');
+let password = 'Пароль';
+let password2 = prompt('Введите пароль');
 if (password === password2) {
-    alert ('Пароль верный');
+    alert('Пароль верный');
 } else {
-    alert ('Пароль не верный');
+    alert('Пароль не верный');
 }
 
 let c = -3.2;
 if (c > 0 && c < 10) {
-    alert ('Верно');
+    alert('Верно');
 } else {
-    alert ('Не верно');
+    alert('Не верно');
 }
 
 let d = 32;
 let e = 130;
-if(d > 100 || e > 100) {
-    alert ('верно');
+if (d > 100 || e > 100) {
+    alert('верно');
 } else {
-    alert ('не верно');
+    alert('не верно');
 }
 
 let a = '2';
 let b = '3';
 // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
 alert(Number(a) + Number(b));
-*/
+
 let monthNumber = 4;
 switch (monthNumber) {
-    case (monthNumber > 11 && monthNumber < 13) || (monthNumber > 0 && monthNumber < 3):
+    case 12:
+    case 1:
+    case 2:
         alert('это весна');
         break;
     case 3:
-        case 4:
-        case 5:
+    case 4:
+    case 5:
         alert('это весна');
         break;
-    case monthNumber > 5 && monthNumber < 9:
+    case 6:
+    case 7:
+    case 8:
         alert('это лето');
         break;
-    case monthNumber > 8 && monthNumber < 12:
+    case 9:
+    case 10:
+    case 11:
         alert('это осень');
         break;
     default:
